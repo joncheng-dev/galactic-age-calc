4b5a2b6faeba7d4616d22b36b332a4b74fe0675f
Jonathan Cheng <jcheng461@gmail.com>
2023-09-01 13:16:50 -0700

    pass test 4 for util. fxn earthYearsDifference

--- src/js/utilityFxns.js
+++ src/js/utilityFxns.js
@@ -1,5 +1,8 @@
 export function earthYearsDifference(presentAge, pastAge) {
-  if (presentAge <= 0 || pastAge <= 0) {
+  console.log(presentAge);
+  if (typeof presentAge !== "number") {
+    return null;
+  } else if (presentAge <= 0 || pastAge <= 0) {
     return null;
   } else {
     let differenceInYears = presentAge - pastAge;

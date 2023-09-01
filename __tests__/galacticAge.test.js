79a598373c3e3ecc6d67f673a5c6bbfab117074d
Jonathan Cheng <jcheng461@gmail.com>
2023-09-01 10:39:27 -0700

    install babel. set up test 1 for GalacticAge class. good fail

---
+++ __tests__/galacticAge.test.js
@@ -0,0 +1,13 @@
+import GalacticAge from "../src/js/galacticAge.js";
+
+describe("GalacticAge", () => {
+  test("should correctly generate an instance of GalacticAge class, taking a parameter of Earth years, setting its value to property 'this.earthYears', and all other properties' values set to 'null'", () => {
+    let usersEarthAge = 50;
+    const newInstance = new GalacticAge(usersEarthAge);
+    expect(newInstance.mercuryYears).toEqual(null);
+    expect(newInstance.earthYears).toEqual(50);
+    expect(newInstance.venusYears).toEqual(null);
+    expect(newInstance.marsYears).toEqual(null);
+    expect(newInstance.jupiterYears).toEqual(null);
+  });
+});

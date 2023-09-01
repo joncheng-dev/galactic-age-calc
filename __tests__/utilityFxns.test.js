3eb45dc10745bec8f48fbe002a6568bd630e4648
Jonathan Cheng <jcheng461@gmail.com>
2023-09-01 13:08:51 -0700

    set up test3 for util. fxn earthYearsDifference

--- __tests__/utilityFxns.test.js
+++ __tests__/utilityFxns.test.js
@@ -8,9 +8,16 @@ describe("earthYearsDifference utility function", () => {
     expect(difference).toEqual(13);
   });
 
-  test("should take in two parameters, presentAge and pastAge, and return null if presentAge is not > 1", () => {
+  test("should take in two parameters, presentAge and pastAge, and return null if presentAge <= 0", () => {
     let ageNow = 0;
     let ageThen = 5;
+    let difference = earthYearsDifference(ageNow, ageThen);
+    expect(difference).toEqual(null);
+  });
+
+  test("should take in two parameters, presentAge and pastAge, and return null if pastAge <= 0", () => {
+    let ageNow = 1;
+    let ageThen = -1;
     let difference = earthYearsDifference(ageNow, ageThen);
     expect(difference).toEqual(null);
   });
